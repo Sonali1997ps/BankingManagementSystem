f21f876da13c0ecb4c08c9011d06390c1e431bd1
vruttantp <vruttantp@MAVCHN0522081.mavericsystems.lan>
2023-10-19 11:55:43 +0530

    db file updated

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -1,21 +1,42 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class DBConnection {
 
 	private Connection conn;
+	private Statement stmt;
 	
-	public static Connection mysqlConnection() throws SQLException {
-		Connection conn = null;
+	public DBConnection() throws SQLException {
+		super();
 		String username = "root";
 		String password = "root";
-
 		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", username, password);
-		return conn;
+		this.stmt = conn.createStatement();
+		
+		System.out.println("DB Connected");
+	}
 
+	public Connection getConn() {
+		return conn;
+	}
+
+	public void setConn(Connection conn) {
+		this.conn = conn;
+	}
+
+	public Statement getStmt() {
+		return stmt;
+	}
+
+	public void setSt(Statement stmt) {
+		this.stmt = stmt;
 	}
 
 	public void closeCon() throws SQLException {
@@ -29,4 +50,31 @@ public class DBConnection {
 
 	}
 
+	public ResultSet getResultSet(String tableName, String columnName, String value) throws SQLException {
+        ArrayList columnNames = new ArrayList();
+        ArrayList data = new ArrayList();
+		ResultSet rs=stmt.executeQuery("select * from " + tableName + " where " + columnName + " = " + value);
+
+	
+		return rs;
+//		ResultSetMetaData md = rs.getMetaData();
+//        int columns = md.getColumnCount();
+//        //  Get column names
+//        for (int i = 1; i <= columns; i++)
+//        {
+//            columnNames.add( md.getColumnName(i) );
+//        }
+//        //  Get row data
+//        while (rs.next())
+//        {
+//            ArrayList row = new ArrayList(columns);
+//
+//            for (int i = 1; i <= columns; i++)
+//            {
+//                row.add( rs.getObject(i));
+//            }
+//            System.out.println(row);
+////            data.add(row);
+//        }
+	}
 }
