384d94e56f897351c1c680090296bdc269e4ff33
vruttantp <vruttantp@MAVCHN0522081.mavericsystems.lan>
2023-10-19 22:16:14 +0530

    CustomerDAO, EmployeeDAO impl

--- src/controller/EmployeeController.java
+++ src/controller/EmployeeController.java
@@ -1,25 +1,27 @@
 package controller;
 
+import java.sql.SQLException;
+
+import dao.EmployeeDAO;
+import dao.EmployeeDAOImpl;
 import model.Employee;
 
 public class EmployeeController {
-	
-	public Employee createEmployee(){
-		return null;
-		
+	EmployeeDAO employeeDAOImpl = new EmployeeDAOImpl();
+	public int createEmployee(Employee employee) throws SQLException{
+		return employeeDAOImpl.createEmployee(employee);		
 	}
 	
-	public boolean updateEmployee(){
-		return false;
-		
+	public int updateEmployee(String columnName, String value) throws SQLException{
+		return employeeDAOImpl.updateEmployee(columnName, value);		
 	}
 	
-	public boolean removeEmployee(){
-		return false;
-		
+	public void removeEmployee(String employeeId) throws SQLException{
+		employeeDAOImpl.removeEmployee(employeeId);
 	}
 	
 	public boolean setManager(){
+//		To be implemented in service class
 		return false;
 		
 	}
