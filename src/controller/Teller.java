fd51f09973b341c06b5ff3cba0df3a72683ad77f
ayesha shaikh <ayeshashaikh7728@gmail.com>
2023-10-20 14:57:31 +0530

    aligment

--- src/controller/Teller.java
+++ src/controller/Teller.java
@@ -2,15 +2,15 @@ package controller;
 
 
 	import java.io.BufferedReader;
-	import java.io.IOException;
-	import java.io.InputStreamReader;
-	import java.sql.Connection;
-	import java.sql.DriverManager;
-	import java.sql.PreparedStatement;
-	import java.sql.ResultSet;
-	import java.sql.SQLException;
-	import java.sql.Statement;
-	import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 	//import JDBCConnection.JDBCoprations;
 
